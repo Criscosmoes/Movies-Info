21c6d587f5917ef27b3307fa4fd72501458b0981
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2021-04-27 13:07:04 -0700

    removed footer from popular tab

--- movies-db-app/src/components/App.js
+++ movies-db-app/src/components/App.js
@@ -1,87 +1,76 @@
-import React, {useEffect} from 'react'; 
-import '../App.css'; 
+import React, { useEffect } from "react";
+import "../App.css";
 
-import NavBar from './NavBar';
-import HomePage from './HomePage';
-import Search from './Search';
-import WatchTrailerv2 from './WatchTrailerv2';
-import Footer from './Footer';
+import NavBar from "./NavBar";
+import HomePage from "./HomePage";
+import Search from "./Search";
+import WatchTrailerv2 from "./WatchTrailerv2";
+import Footer from "./Footer";
 
-import { Switch, Route } from 'react-router-dom'; 
+import { Switch, Route } from "react-router-dom";
 
+import { fetchMovies } from "./../actions/index";
+import { connect } from "react-redux";
 
-import { fetchMovies } from './../actions/index';
-import { connect } from 'react-redux'; 
+const App = ({ movies, fetchMovies }) => {
+  useEffect(() => {
+    fetchMovies("top rated", "movie/top_rated");
+    fetchMovies("trending", "trending/all/week");
+    fetchMovies("trendingToday", "trending/all/day");
+    fetchMovies("popular", "movie/popular");
+    fetchMovies("now_playing", "movie/now_playing");
+    fetchMovies("upcoming", "movie/upcoming");
+  }, [fetchMovies]);
 
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" exact>
+          <NavBar />
+          {
+            <HomePage
+              trending={movies.trending}
+              topRated={movies.topRated}
+              trendingToday={movies.trendingToday}
+            />
+          }
+          <Footer />
+        </Route>
 
+        <Route path="/popular">
+          <NavBar />
+          <Search movies={movies.popular} />
+        </Route>
 
+        <Route path="/now_playing">
+          <NavBar />
+          <Search movies={movies.now_playing} />
+        </Route>
 
+        <Route path="/upcoming">
+          <NavBar />
+          <Search movies={movies.upcoming} />
+        </Route>
 
-const App = ({movies, fetchMovies}) => {
+        <Route path="/movie/:id">
+          <NavBar />
+          <WatchTrailerv2 />
+          <Footer />
+        </Route>
 
+        <Route path="/search/:search">
+          <NavBar />
+          <Search movies={movies.search} />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
-    useEffect(() => {
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
 
-       fetchMovies('top rated', 'movie/top_rated'); 
-       fetchMovies('trending', 'trending/all/week'); 
-       fetchMovies('trendingToday', 'trending/all/day')
-       fetchMovies('popular', 'movie/popular'); 
-       fetchMovies('now_playing', 'movie/now_playing'); 
-       fetchMovies('upcoming', 'movie/upcoming')
-
-
-    }, [fetchMovies])
-
-    return (
-        <div className="App">
-            <Switch>
-                <Route path="/" exact>
-                    <NavBar /> 
-                    {<HomePage trending={movies.trending} topRated={movies.topRated} trendingToday={movies.trendingToday} />}
-                    <Footer />
-                </Route>
-
-                <Route path="/popular" >
-                    <NavBar /> 
-                    <Search movies={movies.popular} />
-                    <Footer />
-                </Route>
-
-                <Route path="/now_playing" >
-                    <NavBar /> 
-                    <Search movies={movies.now_playing} />
-                    <Footer />
-                    
-                </Route>
-
-                <Route path="/upcoming" >
-                    <NavBar /> 
-                    <Search movies={movies.upcoming} />
-                    <Footer />
-                </Route>
-
-                <Route path="/movie/:id">
-                    <NavBar /> 
-                    <WatchTrailerv2 />
-                    <Footer />
-                </Route>
-
-                <Route path="/search/:search">
-                    <NavBar /> 
-                    <Search movies={movies.search} />
-                </Route>
-            </Switch>
-        </div>
-    )
-}
-
-
-const mapStateToProps = state => {
-
-    return { 
-        movies: state.movies, 
-    }
-}
-
-
-export default connect(mapStateToProps, { fetchMovies })(App); 
+export default connect(mapStateToProps, { fetchMovies })(App);
